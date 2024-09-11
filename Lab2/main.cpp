5a61c0d139559bbe5c8b92a88d80d6027f62aa2b
Garrett Foushay <gfoushay001@student.butte.edu>
2024-09-11 03:43:12 -0700

    Lab 2 Submission

---
+++ Lab2/main.cpp
@@ -0,0 +1,42 @@
+//Garrett Foushay
+//9/11
+//Use print statements to output ascii art to the console
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //The image is based of of the old Apple Mac, now refered to as Mac Classic,
+    //And I have the Mac displaying the "Hello World" text typically used for a
+    //First program in a new language
+    
+    //The image does not look quite right because I wanted to use \,
+    //And in order to do so, \\ must be used in the string
+    cout << " ------------------------------------------- " << endl;
+    cout << "|                                           |" << endl;
+    cout << "|    -----------------------------------    |" << endl;
+    cout << "|   |  _   _  _____ _      _     _____  |   |" << endl;
+    cout << "|   | | | | ||  ___| |    | |   |  _  | |   |" << endl;
+    cout << "|   | | |_| || |__ | |    | |   | | | | |   |" << endl;
+    cout << "|   | |  _  ||  __|| |    | |   | | | | |   |" << endl;
+    cout << "|   | | | | || |___| |____| |___\\ \\_/ / |   |" << endl;
+    cout << "|   | \\_| |_/\\____/\\_____/\\_____/\\___/  |   |" << endl;
+    cout << "|   |  _    _  ___________ _    ______  |   |" << endl;
+    cout << "|   | | |  | ||  _  | ___ \\ |   |  _  \\ |   |" << endl;
+    cout << "|   | | |  | || | | | |_/ / |   | | | | |   |" << endl;
+    cout << "|   | | |/\\| || | | |    /| |   | | | | |   |" << endl;
+    cout << "|   | \\  /\\  /\\ \\_/ / |\\ \\| |___| |/ /  |   |" << endl;
+    cout << "|   |  \\/  \\/  \\___/\\_| \\_\\_____/___/   |   |" << endl;
+    cout << "|   |                                   |   |" << endl;
+    cout << "|    -----------------------------------    |" << endl;
+    cout << "|                                           |" << endl;
+    cout << "|                                           |" << endl;
+    cout << "|   ------                   ------------   |" << endl;
+    cout << "|                                           |" << endl;
+    cout << " ------------------------------------------- " << endl;
+    cout << "   |                                     |   " << endl;
+    cout << "    -------------------------------------    " << endl;
+    
+    return 0;
+}
